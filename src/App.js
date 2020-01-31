2da797fbe12c1aaf3c7b5019aeb62a952b548c4f
wildcard329 <enkiduwildman@gmail.com>
2020-01-31 09:36:58 -0800

    added routes

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from "react";
+import { Route } from "react-router-dom";
 import Header from "./components/Header.js";
-
+import { WelcomePage, CharacterList, Navigation } from "./components";
 
 export default function App() {
   return (
