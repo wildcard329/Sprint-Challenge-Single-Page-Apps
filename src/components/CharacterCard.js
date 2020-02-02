f566b20ab5250965e248d4d87a79ca41b641fd80
wildcard329 <enkiduwildman@gmail.com>
2020-02-02 11:13:26 -0800

    added reactstrap styling to cards

--- src/components/CharacterCard.js
+++ src/components/CharacterCard.js
@@ -1,12 +1,28 @@
 import React from "react";
+import {
+  Card,
+  CardTitle,
+  CardText,
+  CardImg,
+  CardBody
+} from 'reactstrap';
 
 
 export default function CharacterCard(props) {
   return (
-    <div className="card">
-        <h2>{props.char.name}</h2>
-        <h2>{props.char.gender}</h2>
-        <img src={props.char.image} />
+    <div className="grid-view">
+      <Card>
+        <CardBody style={{borderRadius: "5%", backgroundColor:"black"}}>
+          <Card style={{backgroundColor: "lightgreen"}}>
+            <CardTitle>{props.char.name}</CardTitle>
+            <CardImg src={props.char.image} />
+            <CardText>Name: {props.char.name}</CardText>
+            <CardText>Gender: {props.char.gender}</CardText>
+            <CardText>Status: {props.char.status}</CardText>
+            <CardText>Species: {props.char.species}</CardText>
+          </Card>
+        </CardBody>
+      </Card>
     </div>
     
     )
